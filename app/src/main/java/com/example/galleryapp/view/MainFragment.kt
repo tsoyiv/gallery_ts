b1f73a651cd543991a1c739abe71bdf4fd43093e
Vlad <tsoiivv@gmail.com>
2023-07-25 23:13:21 +0600

    initial commit, added main fragment

---
+++ app/src/main/java/com/example/galleryapp/view/MainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.galleryapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.galleryapp.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding : FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
